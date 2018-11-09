cc7b5bc7bb8b35592f48478c4a9516b2eb608d69
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-09 17:58:24 +0800

    IMPORTANT 解析 request 資料

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// parser 要在全部的 route handler 之前
+// bodyParser.urlencoded() 會自己 parse 完之後呼叫 next();
+// 不能 parse 全部的東西
+// json, file 那些的要用其他的 parser
+/* ====================================================== */
+// 設定 { extended: false }
+// should be able to parse non-default features
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/', (req, res, next) => {
-  console.log('Always run this line');
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continue to the next middleware in line
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware');
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express!!!</h1>');
 });
 
