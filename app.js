eacd9728bdcdbaee05aa2ecc4748f1a7faa7d345
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-09 15:40:14 +0800

    簡單回傳

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In another middleware');
-  // ... sending responds
+  res.send('<h1>Hello from express!!!</h1>');
 });
 
 const server = http.createServer(app);
