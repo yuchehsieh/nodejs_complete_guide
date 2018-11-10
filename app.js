f995e592678ae901bda975b3fdb5e9adb3df510c
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-10 16:22:37 +0800

    新增 404 Page

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found !!!</h1>');
+});
+
 app.listen(2000);
 
 // 等同於：
