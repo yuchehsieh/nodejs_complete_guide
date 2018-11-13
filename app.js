5d40737ddab3bcc8133242c651159576d430523a
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-13 11:28:10 +0800

    404.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const path = require('path');
 
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -21,7 +22,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found !!!</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(2000);
