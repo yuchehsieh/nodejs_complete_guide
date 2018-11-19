5b7ee0a848a869e0aac744832d066d79e3d89990
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-19 12:27:43 +0800

    [IMPORTANT] 建立基本 controllers 並依資料拆分程式碼

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
@@ -12,18 +11,6 @@ const router = express.Router();
 // 且定義 views 在 views 資料夾下，所以也不用去建立路徑到那個檔案
 // 也不用寫副檔名 (.pug) 因為定義 pug 是 default templating engine
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-    // layout: false 不使用 layout 如果前面有設定這邊預設是 true
-  });
-  // 傳送物件包含 key: name 比較好去 reference
-});
+router.get('/', productsController.getProduct);
 
 module.exports = router;
