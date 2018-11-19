2f6f1c62bcb723685a6df4be53efa81d8125579b
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-19 17:27:51 +0800

    重複使用 models 裡面的程式碼

--- models/product.js
+++ models/product.js
@@ -2,6 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -9,13 +19,7 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(rootDir, 'data', 'products.json');
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -24,12 +28,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
