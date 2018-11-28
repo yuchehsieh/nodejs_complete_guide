29d5991a46c2350a4e76b35bca100b7813a85e30
yuchehseih <hsiehdanny860605@gmail.com>
2018-11-28 10:07:11 +0800

    刪除購物車裡的商品，價格/數量 記得考慮進去

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const rootDir = require('../util/path');
 const p = path.join(rootDir, 'data', 'products.json');
 
@@ -45,10 +47,12 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
       const updatedProduct = products.filter(prod => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProduct), err => {
         if (!err) {
           // 同時也刪除購物車裡的商品
+          Cart.deleteProduct(id, product.price);
         }
       });
     });
