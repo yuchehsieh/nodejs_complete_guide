338019e341a8f9acbb2e8ba3066be628ff2ce702
yuchehseih <hsiehdanny860605@gmail.com>
2019-01-03 15:22:48 +0800

    [IMPORTANT] 安裝 sequelize 以及連上資料庫要注意的地方

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'murphy0605'
+const sequelize = new Sequelize('node-complete', 'root', 'murphy0605', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: 'murphy0605'
+// });
+
+// module.exports = pool.promise();
