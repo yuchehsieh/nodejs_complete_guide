422546a61afecd432a265d3bc12e9fa1e52b5fbf
yuchehseih <hsiehdanny860605@gmail.com>
2019-01-08 14:43:53 +0800

    [VERY IMPORTANT] 一對一 多對一 的資料庫關係，應用在 product 和 cart 透過  cartItem 連接

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -67,11 +69,15 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true })
+  .sync({ force: true })
   // 一直 call force: true 會洗掉資料
-  .sync()
+  // .sync()
   .then(result => {
     return User.findById(1);
   })
